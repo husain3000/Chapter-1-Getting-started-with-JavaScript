4ebc6e2c9e37c49bb428f574800cfbe9b4ca324d
husain3000 <73273019+husain3000@users.noreply.github.com>
2023-02-17 14:55:29 +0530

    first commit

---
+++ Using the DOM API/using the dom api.js
@@ -0,0 +1,5 @@
+// document.getElementById("paragraph").textContent="Hello, World";
+
+var element = document.createElement('b');
+element.textContent ="Hello, World";
+document.body.appendChild(element);
